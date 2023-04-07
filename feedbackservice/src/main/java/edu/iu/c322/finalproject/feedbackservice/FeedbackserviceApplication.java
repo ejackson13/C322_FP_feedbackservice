dc43115377365bafffd34225cd5a4e2a24de58ac
Evan Jackson <evanjack@iu.edu>
2023-04-07 14:55:19 -0400

    Initial commit

---
+++ feedbackservice/src/main/java/edu/iu/c322/finalproject/feedbackservice/FeedbackserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.finalproject.feedbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackserviceApplication.class, args);
+	}
+
+}
---
+++ feedbackservice/src/test/java/edu/iu/c322/finalproject/feedbackservice/FeedbackserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.finalproject.feedbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
